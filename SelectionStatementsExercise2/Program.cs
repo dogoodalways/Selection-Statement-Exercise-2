43d63b42053fc1239822f7d517faf652ae5fd5f3
yosefamiami@gmail.com <yosefamiami@gmail.com>
2023-03-16 17:38:16 -0400

    switch statements exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var userAnswer = Console.ReadLine();
+
+            switch(userAnswer.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("You like logic.");
+                        break;
+                case "literature":
+                    Console.WriteLine("You like stories.");
+                    break;
+                case "science":
+                    Console.WriteLine("You like facts");
+                    break;
+                case "hisotry":
+                    Console.WriteLine("You like events");
+                    break;
+                case "anatomy":
+                    Console.WriteLine("You like to know yourself");
+                    break;
+
+                default: 
+                    Console.WriteLine("You wish you liked something");
+                    break;
+
+
+            }
         }
     }
 }
